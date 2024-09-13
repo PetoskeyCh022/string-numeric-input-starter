8071d7e57aa15a779b9872db22f7c31080b22616
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2024-09-13 10:40:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-# Student name
-# Current date
+# Charles Petoskey
+# 9/13/24
 # String & Numeric Input 
 
 # Task 1
-# Comment
-# Comment
+#first_name = input('What is your first name?: ')
+#program_attendence = input('What program do you attend at Career Tech?: ')
+#print('Your name is ' + first_name + ', and you attend ' + program_attendence + '.')
+
 
 
 
 
 # Task 2
-# Comment
-# Comment
+#age = int(input('What is your age?:')) + 10
+#print('You are ' + str(age) + ' years old.')
+
 
 
 
 
 # Task 3
-# Comment
-# Comment
+#num_of_people_in_family = int(input('How many people are in your family?: '))
+#print('You have '+str(num_of_people_in_family) + ' people in your family.')
+#print(f'You have {num_of_people_in_family} people in your family.')
+
 
 
 
 
 # Task 4
-# Comment
-# Comment
+#height = float(input('How tall are you?: '))
+#conversion_factor = height * 2.54
+#print('You are ' + str(conversion_factor) + ' centimeters tall!')
+#print(f'You are {conversion_factor} centimeters tall!')
+
 
 
 
